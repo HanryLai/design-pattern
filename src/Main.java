37315bcaf4bf151c0e7ce174a2ef5eb756700efd
Hieu Lai <ldmhieudev@gmail.com>
2025-03-04 19:28:31 +0700

    State,stategy, decorator

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import Decorator.BaseTask;
+import Decorator.DirectorDecorator;
+import Decorator.Task;
+import Strategy.EmployeeStrategy;
+import Strategy.EmployeeStrategyContext;
+import state.DirectorState;
+import state.EmployeeStateContext;
+
 public class Main {
     public static void main(String[] args) {
+        // Approach 1: No pattern
         Employee emp1 = new Employee("GIAMDOC");
         emp1.handleRequest();
 
         Employee emp2 = new Employee("NHANVIEN");
         emp2.handleRequest();
+        System.out.println("=== Employee Tasks Examples in Different Approaches ===");
+
+        // Approach 2: State Pattern
+        System.out.println("\n-- Approach 2: State Pattern");
+        EmployeeStateContext employeeStateContext = new EmployeeStateContext(new DirectorState());
+        employeeStateContext.printTasks();
+
+        System.out.println("\n-- Approach 3: Decorator Pattern");
+        Task baseTask = new BaseTask();
+        Task directorTask = new DirectorDecorator(baseTask);
+        System.out.println("Decorator Pattern Approach => Tasks: " + directorTask.getTaskDescription());
+
+        // Approach 4: Strategy Pattern
+        System.out.println("\n-- Approach 4: Strategy Pattern");
+        EmployeeStrategyContext employeeStrategyContext = new EmployeeStrategyContext(new EmployeeStrategy());
+        employeeStrategyContext.printTasks();
     }
 }
