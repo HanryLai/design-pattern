f2ab6f40c549d068ffcc5560b70c1d9a656c9d66
Hieu Lai <ldmhieudev@gmail.com>
2025-04-08 20:05:40 +0700

    Tuan 5

---
+++ tuan_5/microservice_spring/src/main/java/iuh/fit/microservice_spring/MicroserviceSpringApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.microservice_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSpringApplication.class, args);
+    }
+
+}
